6ad7dd81e7797e0b3b8b3c815d86d7422d02e6cd
Konrad Wisniewski <konrad.m.wisniewski@gmail.com>
2018-10-03 23:56:16 -0600

    figuring shit out

--- Ass 1/q4.py
+++ Ass 1/q4.py
@@ -1,15 +1,26 @@
 def some_hash(val):
     retval = [0x0f, 0xff, 0x00]
-    for i in val:
-        n = ord(i)
-        retval[0] ^= n
+    retvalI = [0x0f, 0xff, 0x00]
+
+    for i in val: # Loop for each char in string
+        print (retval)
+
+        n = ord(i) # get unicode representation
+        #print (n, i)    
+
+        retval[0] ^= n     
         retval[1] &= n
         retval[2] |= n
-        print bin(n), retval
+        print (bin(n), retval)
+
     return '' + chr(retval[0]) + chr(retval[1]) + chr(retval[2])
 
-x = some_hash("123")
+x = some_hash("a2dfasdfasf")
 print (x)
 
-x = some_hash("123123123")
-print (x)
+
+y = some_hash("asdfasdfasf")
+print (y)
+
+if (x == y):
+    print ("Hashes are identical bro")
